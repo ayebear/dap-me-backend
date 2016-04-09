2a1e6cca68afa031850c6b5b025f4800bf878840
Eric Hebert <ayebear@gmail.com>
2016-04-09 05:35:12 -0400

    Fixed double-send issue Refactored some code

--- index.js
+++ index.js
@@ -2,36 +2,47 @@ var io = require('socket.io')();
 
 var clients = {};
 
-function push_data() {
+function get_data() {
 	// Get data to send
 	var data = {};
 	for (var client_id in clients) {
 		data[client_id] = clients[client_id].data;
 	}
+	return data;
+}
+
+function push(client_id, data) {
+	clients[client_id].socket.emit('user_data', data);
+}
+
+function push_to_all() {
+	var data = get_data();
 
 	// Send data to all clients
 	for (var client_id in clients) {
-		clients[client_id].socket.emit('user_data', data);
+		push(client_id, data);
 	}
 }
 
 io.on('connection', function(socket) {
+
+	socket.emit('user_data', get_data());
+
 	clients[socket.id] = {
 		socket: socket,
 		data: {}
 	};
-	push_data();
 
 	socket.on('user_data', function(data) {
 		console.log("Received data from: " + socket.id);
 		console.log(data);
 		clients[socket.id].data = data;
-		push_data();
+		push_to_all();
 	});
 
 	socket.on('disconnect', function() {
 		delete clients[socket.id];
-		push_data();
+		push_to_all();
 	});
 });
 
