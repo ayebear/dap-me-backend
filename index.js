d49a14dac156ce9ad86e5fb050e6efe28b181d48
Eric Hebert <ayebear@gmail.com>
2016-04-09 04:23:25 -0400

    Updated server to handle user data Also updates all clients when data changes

--- index.js
+++ index.js
@@ -2,18 +2,36 @@ var io = require('socket.io')();
 
 var clients = {};
 
-io.on('connection', function(socket){
-	clients[socket.id] = socket;
-	for (var key in clients) {
-		console.log(key);
+function push_data() {
+	// Get data to send
+	var data = {};
+	for (var client_id in clients) {
+		data[client_id] = clients[client_id].data;
 	}
 
-	socket.on('event', function(data) {
+	// Send data to all clients
+	for (var client_id in clients) {
+		clients[client_id].socket.emit('user_data', data);
+	}
+}
+
+io.on('connection', function(socket) {
+	clients[socket.id] = {
+		socket: socket,
+		data: {}
+	};
+	push_data();
+
+	socket.on('user_data', function(data) {
 		console.log("Received data from: " + socket.id);
 		console.log(data);
+		clients[socket.id].data = data;
+		push_data();
 	});
+
 	socket.on('disconnect', function() {
 		delete clients[socket.id];
+		push_data();
 	});
 });
 
